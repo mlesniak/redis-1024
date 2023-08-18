80646df2a85fc849871aeebc98c5435750fe5b8a
Michael Lesniak <mail@mlesniak.com>
2023-08-18 21:07:13 +0200

    Add listener for TCP connections

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Redis in 1024 lines of code
 
+## Goal
+
+Implement as much functionality of a redis server as possible in 1024 lines of 
+C# code (according to `cloc`).
+
+## Approach
+
+- Listen to connection
+- Implement commands until we run out of lines
+
 ## References
 
  - https://redis.io/docs/reference/protocol-spec/
--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+
+TcpListener server = new(6379);
+server.Start();
+Console.WriteLine("Server started");
+while (true)
+{
+    TcpClient client = server.AcceptTcpClient();
+    Console.WriteLine("Client connected");
+    var stream = client.GetStream();
+
+    byte[] buffer = new byte[1024];
+    int read = stream.Read(buffer);
+    foreach (byte b in buffer.Take(read))
+    {
+        Console.Write((char)b);
+    }
+    Console.WriteLine();
+    client.Close();
+}
