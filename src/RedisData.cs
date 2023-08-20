0081af15277e0046f9a922477d2a8103e500caa7
Michael Lesniak <mail@mlesniak.com>
2023-08-20 20:41:20 +0200

    Refactoring

--- src/RedisData.cs
+++ src/RedisData.cs
@@ -2,6 +2,19 @@ using System.Text;
 
 namespace Lesniak.Redis;
 
+public static class DataTypeIdentifier
+{
+    public static char Identifier(this RedisData.DataType type)
+    {
+        return type switch
+        {
+            RedisData.DataType.Array => '*',
+            RedisData.DataType.BulkString => '$',
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
+}
+
 public class RedisData
 {
     public enum DataType
@@ -11,12 +24,12 @@ public class RedisData
     }
 
     public DataType Type { get; set; }
-    public string? BulkString { get; set; } = null;
-    public List<RedisData>? ArrayValues { get; set; } = null;
+    public string? BulkString { get; set; }
+    public List<RedisData>? ArrayValues { get; set; }
 
     public static RedisData Parse(byte[] data)
     {
-        var (result, _) = Parse(data, 0);
+        (RedisData result, _) = Parse(data, 0);
         return result;
     }
 
@@ -25,34 +38,35 @@ public class RedisData
     {
         RedisData result = new();
         int nextOffset;
-        
-        if (data[offset] == '*')
+
+        switch (data[offset])
         {
-            result.Type = DataType.Array;
-            result.ArrayValues = new();
-            var numElementsIndexEnd = Array.IndexOf(data, (byte)'\r', offset);
-            var numElements = Int32.Parse(Encoding.ASCII.GetString(data, offset + 1, numElementsIndexEnd - offset - 1));
-            offset = numElementsIndexEnd + 2;
-            for (var i = 0; i < numElements; i++)
-            {
-                (RedisData elem, int nextArrayOffset) = Parse(data, offset);
-                result.ArrayValues.Add(elem);
-                offset = nextArrayOffset;
-            }
-            nextOffset = offset;
-        }
-        else if (data[offset] == '$')
-        {
-            result.Type = DataType.BulkString;
-            var lengthEnd = Array.IndexOf(data, (byte)'\r', offset);
-            var length = Int32.Parse(Encoding.ASCII.GetString(data, offset + 1, lengthEnd - offset - 1));
-            int stringStart = lengthEnd + 2;
-            result.BulkString = Encoding.ASCII.GetString(data, stringStart, length);
-            nextOffset = stringStart + length + 2;
-        }
-        else
-        {
-            throw new ArgumentException($"Invalid byte {data[offset]} to parse");
+            case (byte)'$':
+                result.Type = DataType.BulkString;
+                var lengthEnd = Array.IndexOf(data, (byte)'\r', offset);
+                var length = Int32.Parse(Encoding.ASCII.GetString(data, offset + 1, lengthEnd - offset - 1));
+                int stringStart = lengthEnd + 2;
+                result.BulkString = Encoding.ASCII.GetString(data, stringStart, length);
+                nextOffset = stringStart + length + 2;
+                break;
+            case (byte)'*':
+                result.Type = DataType.Array;
+                result.ArrayValues = new();
+                var numElementsIndexEnd = Array.IndexOf(data, (byte)'\r', offset);
+                var numElements =
+                    Int32.Parse(Encoding.ASCII.GetString(data, offset + 1, numElementsIndexEnd - offset - 1));
+                offset = numElementsIndexEnd + 2;
+                for (var i = 0; i < numElements; i++)
+                {
+                    (RedisData elem, int nextArrayOffset) = Parse(data, offset);
+                    result.ArrayValues.Add(elem);
+                    offset = nextArrayOffset;
+                }
+
+                nextOffset = offset;
+                break;
+            default:
+                throw new ArgumentException($"Invalid byte {data[offset]} to parse");
         }
 
         return (result, nextOffset);
@@ -60,7 +74,6 @@ public class RedisData
 
     public override string ToString()
     {
-        return "TODO"; 
+        return "TODO";
     }
-    
 }
