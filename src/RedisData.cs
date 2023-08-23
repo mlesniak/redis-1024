7a6d44ff625ac99654792823573bcea735cdb6a5
Michael Lesniak <mail@mlesniak.com>
2023-08-23 20:38:28 +0200

    Use dictionary to organize parsers per byte

--- src/RedisData.cs
+++ src/RedisData.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
+using System.Reflection.PortableExecutable;
 using System.Text;
 
 namespace Lesniak.Redis;
-
 
 public class RedisData
 {
@@ -12,51 +12,39 @@ public class RedisData
 
     private delegate (RedisData, int) Parser(byte[] data, int offset);
 
-    private static Dictionary<char, Parser> parsers = new();
+    private static Dictionary<byte, Parser> parsers = new();
 
-    public static RedisData Parse(byte[] data)
+    static RedisData()
     {
-        (RedisData result, _) = Parse(data, 0);
-        return result;
-    }
-
-    static (RedisData, int) Parse(byte[] data, int offset)
-    {
-        RedisData result = new();
-        int nextOffset;
-
-        switch (data[offset])
+        parsers.Add((byte)'$', (data, offset) =>
         {
-            case var b when b == RedisDataType.BulkString.Identifier():
-                result.Type = RedisDataType.BulkString;
-                var lengthEnd = Array.IndexOf(data, (byte)'\r', offset);
-                var length = Int32.Parse(Encoding.ASCII.GetString(data, offset + 1, lengthEnd - offset - 1));
-                int stringStart = lengthEnd + 2;
-                result.BulkString = Encoding.ASCII.GetString(data, stringStart, length);
-                nextOffset = stringStart + length + 2;
-                break;
-            case var b when b == RedisDataType.Array.Identifier():
-                result.Type = RedisDataType.Array;
-                result.ArrayValues = new();
-                var numElementsIndexEnd = Array.IndexOf(data, (byte)'\r', offset);
-                var numElements =
-                    Int32.Parse(Encoding.ASCII.GetString(data, offset + 1, numElementsIndexEnd - offset - 1));
-                offset = numElementsIndexEnd + 2;
-                for (var i = 0; i < numElements; i++)
-                {
-                    (RedisData elem, int nextArrayOffset) = Parse(data, offset);
-                    result.ArrayValues.Add(elem);
-                    offset = nextArrayOffset;
-                }
-
-                nextOffset = offset;
-                break;
-            default:
-                throw new ArgumentException($"Invalid byte {data[offset]} to parse");
-        }
-
-        return (result, nextOffset);
+            RedisData result = new() { Type = RedisDataType.BulkString };
+            var lengthEnd = Array.IndexOf(data, (byte)'\r', offset);
+            var length = Int32.Parse(Encoding.ASCII.GetString(data, offset + 1, lengthEnd - offset - 1));
+            int stringStart = lengthEnd + 2;
+            result.BulkString = Encoding.ASCII.GetString(data, stringStart, length);
+            return (result, stringStart + length + 2);
+        });
+        parsers.Add((byte)'*', (data, offset) =>
+        {
+            RedisData result = new() { Type = RedisDataType.Array, ArrayValues = new() };
+            var numElementsIndexEnd = Array.IndexOf(data, (byte)'\r', offset);
+            var numElements =
+                Int32.Parse(Encoding.ASCII.GetString(data, offset + 1, numElementsIndexEnd - offset - 1));
+            offset = numElementsIndexEnd + 2;
+            for (var i = 0; i < numElements; i++)
+            {
+                (RedisData elem, int nextArrayOffset) = Parse(data, offset);
+                result.ArrayValues.Add(elem);
+                offset = nextArrayOffset;
+            }
+            return (result, offset);
+        });
     }
+
+    public static RedisData Parse(byte[] data) => Parse(data, 0).Item1;
+
+    static (RedisData, int) Parse(byte[] data, int offset) => parsers[data[offset]].Invoke(data, offset);
 
     public override string ToString()
     {
