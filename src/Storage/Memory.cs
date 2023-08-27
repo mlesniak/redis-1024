261b9bfc65704671ec1ac78bb52315c8221a6909
Michael Lesniak <mail@mlesniak.com>
2023-08-27 19:08:18 +0200

    Add thread-safety to memory structure

--- src/Storage/Memory.cs
+++ src/Storage/Memory.cs
@@ -1,9 +1,11 @@
+using System.Collections.Concurrent;
+
 namespace Lesniak.Redis.Storage;
 
 public class Memory
 {
     // We will add expiration time later.
-    protected readonly Dictionary<string, byte[]> _memory = new();
+    private readonly ConcurrentDictionary<string, byte[]> _memory = new();
 
     public void Set(string key, byte[] value)
     {
