904483adf9ab55df754f29a2da22ee55ea734444
Michael Lesniak <mail@mlesniak.com>
2023-09-16 21:19:29 +0200

    Refactor command handling

--- src/Communication/Network/CommandHandler.cs
+++ src/Communication/Network/CommandHandler.cs
@@ -5,55 +5,64 @@ using Lesniak.Redis.Core.Model;
 
 namespace Lesniak.Redis.Communication.Network;
 
-// TODO(mlesniak) Refactoring.
 public class CommandHandler
 {
     private readonly IDatabase _database;
 
     public CommandHandler(IDatabase database)
     {
-    _database = database;
+        _database = database;
     }
-    
+
+    // Commands are send as serialized arrays, which is
+    // the main reason we handle them as such internally
+    // as well.
     public byte[] Execute(RedisArray commandline)
     {
-        List<RedisType> array = commandline.Values!;
-        var command = ((RedisString)array[0]).Value!;
-        switch (command)
+        List<RedisType> commandParts = commandline.Values!;
+        var command = ((RedisString)commandParts[0]).Value!.ToLower();
+        return command switch
         {
-            case "set":
-                var setKey = ((RedisString)array[1]).Value!;
-                byte[] value = Encoding.ASCII.GetBytes(((RedisString)array[2]).Value!);
+            "set" => SetHandler(commandParts),
+            "get" => GetHandler(commandParts),
+            _ => UnknownCommandHandler()
+        };
+    }
 
-                int? expirationInMs = null;
-                if (array.Count > 3)
-                {
-                    var type = ((RedisString)array[3]).Value!;
-                    var num = Int32.Parse(((RedisString)array[4]).Value!);
+    private byte[] SetHandler(IReadOnlyList<RedisType> array)
+    {
+        var setKey = ((RedisString)array[1]).Value!;
+        byte[] value = Encoding.ASCII.GetBytes(((RedisString)array[2]).Value!);
 
-                    expirationInMs = type.ToLower() switch
-                    {
-                        "ex" => num * 1_000,
-                        "px" => num,
-                        _ => throw new ArgumentOutOfRangeException()
-                    };
-                }
-                
-                _database.Set(setKey, value, expirationInMs);
-                return "+OK\r\n"u8.ToArray();
-            case "get":
-                var getKey = ((RedisString)array[1]).Value!;
-                var resultBytes = _database.Get(getKey);
-                if (resultBytes == null)
-                {
-                    return RedisString.Nil().Serialize();
-                }
+        int? expirationInMs = null;
+        if (array.Count > 3)
+        {
+            var type = ((RedisString)array[3]).Value!;
+            var num = Int32.Parse(((RedisString)array[4]).Value!);
 
-                // Create BulkString as response for now.
-                return RedisString.From(resultBytes).Serialize();
-            default:
-                // Ignoring it for now.
-                return "-UNKNOWN COMMAND\r\n"u8.ToArray();
+            expirationInMs = type.ToLower() switch
+            {
+                "ex" => num * 1_000,
+                "px" => num,
+                _ => throw new ArgumentOutOfRangeException()
+            };
         }
+
+        _database.Set(setKey, value, expirationInMs);
+        return "+OK\r\n"u8.ToArray();
+    }
+
+    private byte[] GetHandler(IReadOnlyList<RedisType> commandParts)
+    {
+        var getKey = ((RedisString)commandParts[1]).Value!;
+        var resultBytes = _database.Get(getKey);
+        return resultBytes == null
+            ? RedisString.Nil().Serialize()
+            : RedisString.From(resultBytes).Serialize();
+    }
+
+    private byte[] UnknownCommandHandler()
+    {
+        return "-UNKNOWN COMMAND\r\n"u8.ToArray();
     }
 }
