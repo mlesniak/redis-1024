8a59f488e625dfe62ccb28aba76c1aa2217aeb40
Michael Lesniak <mail@mlesniak.com>
2023-09-17 04:26:56 +0200

    Add real client project

---
+++ client/Program.cs
@@ -0,0 +1,8 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
+IDatabase db = redis.GetDatabase();
+
+db.StringSet("michael", "lesniak");
+string? value = db.StringGet("michael");
+Console.WriteLine($"Value: {value}");
