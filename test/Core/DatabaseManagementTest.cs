40caea8bab8c16408825ede9691320bf7c984af2
Michael Lesniak <mail@mlesniak.com>
2023-10-02 22:07:28 +0200

    Refactoring

--- test/Core/DatabaseManagementTest.cs
+++ test/Core/DatabaseManagementTest.cs
@@ -4,15 +4,13 @@ namespace Lesniak.Redis.Test.Core;
 
 public class DatabaseManagementTest
 {
-    
     private readonly TestConfiguration _configuration = new();
-    private readonly TestDateTimeProvider _dateTimeProvider;
     private readonly IDatabaseManagement _sut;
 
     public DatabaseManagementTest()
     {
-        _dateTimeProvider = new TestDateTimeProvider();
-        _sut = new Database(TestLogger<Database>.Get(), _configuration, _dateTimeProvider);
+        TestDateTimeProvider dateTimeProvider = new();
+        _sut = new Database(TestLogger<Database>.Get(), _configuration, dateTimeProvider);
     }
 
     [Fact]
